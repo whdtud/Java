b1919b77f90f3ce1adf02aac0ced3ec1737f5b6a
WooJongSeong <whdtud@gmail.com>
2021-05-22 14:26:58 +0900

    may22
    N1152 contains problum

--- IntelliJ/Algorithm/src/Main.java
+++ IntelliJ/Algorithm/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        N1157();
+        N1152();
     }
 
     public static void N11720() {
@@ -60,8 +60,7 @@ public class Main {
         }
     }
 
-    public static void N1157()
-    {
+    public static void N1157() {
         /* 단어 공부
          * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된
          * 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
@@ -97,6 +96,27 @@ public class Main {
         }
 
         System.out.print(highestChar);
+
+        sc.close();
+    }
+
+    public static void N1152() {
+        /* 단어의 개수
+           영어 대소문자와 띄어쓰기만으로 이루어진 문자열이 주어진다.
+           이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
+           단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+           입력) 첫 줄에 영어 대소문자와 띄어쓰기로 이루어진 문자열이 주어진다.
+           이 문자열의 길이는 1,000,000을 넘지 않는다. 단어는 띄어쓰 한 개로 구분되며,
+           공백이 연속해서 나오는 경우는 없다. 또한 문자열의 앞과 뒤에는 공백이 있을 수도 있다.
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        input = input.trim();
+        String[] words = input.split(" ");
+
+        System.out.print(words.length);
 
         sc.close();
     }
