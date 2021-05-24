f9441a1085d09e35fe78563b8a5c799715585f4a
WooJongSeong <whdtud@gmail.com>
2021-05-24 17:43:41 +0900

    may24

--- IntelliJ/may24/src/For05.java
+++ IntelliJ/may24/src/For05.java
@@ -8,5 +8,12 @@ public class For05 {
             }
             System.out.println();
         }
+
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+            System.out.println();
+        }
     }
 }
---
+++ IntelliJ/may24/src/Switch02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Switch02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please input your position");
+        System.out.print("1. CEO\t2.department head\t3.section chief\4.deputy section chief");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+
+    }
+}
