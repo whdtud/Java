5999f973369cad3d96c34726fac341f76c718731
WooJongSeong <whdtud@gmail.com>
2021-05-25 17:38:33 +0900

    algorithm 1157

--- EE/Algorithm/src/Main.java
+++ EE/Algorithm/src/Main.java
@@ -44,35 +44,42 @@ public class Main {
 
 	public static void N1157()
 	{
-		/* 단어 공부
-		 * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된
-		 * 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
-		 * 
-		 * 입력) 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
-		 * 출력) 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다.
-		 * 단, 가장 많이 사용된 알파벳이 여러개 존재하는 경우에는 ?를 출력한다.
-		 */
-		
-		Scanner cs = new Scanner(System.in);
-		String input = cs.next();
-		
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for (int i = 0; i < input.length(); i++) {
-			char key = input.charAt(i);
-			Integer value = map.getOrDefault(key, 0);
-			value++;
-		}
-		
-		char highestChar = '0';
-		int highestValue = -1;
-		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-			int value = entry.getValue(); 
-			if (value > highestValue) {
-				highestValue = value;
-				highestChar = entry.getKey();
-			}
-		}
-		
-		
+        /* 단어 공부
+         * 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된
+         * 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+         *
+         * 입력) 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+         * 출력) 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다.
+         * 단, 가장 많이 사용된 알파벳이 여러개 존재하는 경우에는 ?를 출력한다.
+         */
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            Character ch = Character.toUpperCase(input.charAt(i));
+            Integer value = map.getOrDefault(ch, 0);
+            value++;
+
+            map.put(ch, value);
+        }
+
+        int highestValue = -1;
+        char highestChar = '0';
+        for (Map.Entry<Character, Integer> entry :
+                map.entrySet()){
+            int value = entry.getValue();
+            if (value > highestValue) {
+                highestValue = value;
+                highestChar = entry.getKey();
+            } else if (value == highestValue) {
+                highestChar = '?';
+            }
+        }
+
+        System.out.print(highestChar);
+
+        sc.close();
 	}
 }
